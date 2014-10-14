645d82542507a68af879515d1c37ed6b1a110b9b
Barak Shohat <barak@bazzisoft.com>
2014-10-14 22:16:22 +0300

    Improved nativetext test harness
    (cherry picked from commit 47c9394e7541b1e18c1749faeb236ffd5fb1f1e3)

--- nativetext/src/Main.hx
+++ nativetext/src/Main.hx
@@ -6,13 +6,18 @@ import flash.display.Sprite;
 import flash.events.Event;
 import nativetext.event.NativeTextEvent;
 import nativetext.NativeText;
+import nativetext.NativeTextField;
 
 
 class Main extends Sprite implements ITestPanelViewDelegate
 {
 	private static inline var BUTTON_ADD_TEXT_FIELD = "ADD NATIVE TEXT FIELD";
+	private static inline var BUTTON_REMOVE_TEXT_FIELD = "REMOVE NATIVE TEXT FIELD";
 
     private var m_testPanelView:TestPanelView = null;
+	private var m_textFieldStack = new Array<NativeTextField>();
+	private var m_initial_y = 100;
+	
 
 	public function new()
     {
@@ -20,9 +25,9 @@ class Main extends Sprite implements ITestPanelViewDelegate
 
         NativeText.Initialize();
 
-        stage.addEventListener(NativeTextEvent.TEXT_CHANGED, function(e) { trace(e); } );
+        stage.addEventListener(NativeTextEvent.TEXT_CHANGED, function(e) { trace("Stage Event: " + e); } );
 
-        m_testPanelView = new TestPanelView(this, [BUTTON_ADD_TEXT_FIELD]);
+        m_testPanelView = new TestPanelView(this, [BUTTON_ADD_TEXT_FIELD, BUTTON_REMOVE_TEXT_FIELD]);
         addChild(m_testPanelView);
   	}
 
@@ -31,8 +36,26 @@ class Main extends Sprite implements ITestPanelViewDelegate
         switch (button)
         {
 			case BUTTON_ADD_TEXT_FIELD:
-				NativeText.AddTextField(100, 100);
-				m_testPanelView.WriteLine("AddTextField - CALLED");
+			{
+				var tf = new NativeTextField();
+				m_textFieldStack.push(tf);
+				tf.addEventListener(NativeTextEvent.TEXT_CHANGED, function(e) { trace("Textfield #" + tf.eventDispatcherId + ": " + e); } );
+				m_testPanelView.WriteLine("Created text field #" + tf.eventDispatcherId + ".");
+			}
+				
+			case BUTTON_REMOVE_TEXT_FIELD:
+			{
+				if (m_textFieldStack.length > 0)
+				{
+					var tf = m_textFieldStack.pop();
+					m_testPanelView.WriteLine("Removing last text field (#" + tf.eventDispatcherId + ").");
+					tf.Destroy();
+				}
+				else
+				{
+					m_testPanelView.WriteLine("No text fields exist.");
+				}
+			}
         }
     }
 }
