e987bf97783f53b9311c73e636aa0dca61492c09
Barak Shohat <barak@bazzisoft.com>
2014-10-23 16:21:47 +0300

    Fixed broken paths

--- nativetext/run_android.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-SET PATH=%PATH%;c:\Android\AndroidSDK\tools;c:\Android\AndroidSDK\platform-tools
-adb logcat -c
-call lime test project.xml android -debug
-rem call lime test project.xml android
-pause
-pskill adb
